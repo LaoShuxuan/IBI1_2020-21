368d77067d5a3c35f0e5ab7a9e68d08dbce40baf
shuxuan <3200110953@zju.edu.cn>
2021-03-10 11:43:53 +0800

    variables.py

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -9,3 +9,15 @@ elif d==e:
    print("d=e")
 else:
    print("d<e")
+
+X=True
+Y=False
+Z=(X and not Y) or (Y and not X)
+if Z==True:
+   print("verify Z is true if either X or are true")
+W=(X!=Y)
+if Z is not W:
+   print(Z,"is not the same as",W)
+else:
+   print("Z is the same as W")
+   
