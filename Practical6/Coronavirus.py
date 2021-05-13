ce2643efd939fe64a2a17f7568790448d518fe44
LaoShuxuan <79951623+LaoShuxuan@users.noreply.github.com>
2021-05-13 15:13:50 +0800

    Update Coronavirus.py

--- Practical6/Coronavirus.py
+++ Practical6/Coronavirus.py
@@ -10,6 +10,8 @@ sizes = [29862124,11285561,11205972,4360823,4234924]
 explode = (0,0,0,0,0)
 #specifies the fraction of the radius with which to offset each wedge
 plt.pie(sizes,explode=explode,labels=labels,autopct='%1.1f%%',shadow=False,startangle=90)
+#give the pie chart a title
+plt.title('The proportion of infected cases for five countries')
 #Equal aspect ratio ensures that pie is drawn as a circle
 plt.axis('equal')
 #show the plot
